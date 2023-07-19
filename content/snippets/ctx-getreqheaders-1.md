c60974190457e0aea9e5e5d043541b03ed427a2b
kirari04 <lev@saminskij.ch>
2023-07-19 20:37:31 +0200

    added  getreqheaders

---
+++ content/snippets/ctx-getreqheaders-1.md
@@ -0,0 +1,4 @@
+```go
+// Function signature
+func (c *Ctx) GetReqHeaders() map[string]string
+```
---
+++ pages/docs/api/ctx/getreqheaders.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="flex flex-col-reverse md:flex-row grow">
+        <div class="flex flex-col grow">
+            <H1>🧠 Ctx - {{ name }}</H1>
+            <P> Returns the HTTP request headers. </P>
+            <Code :path="`/snippets/ctx-${name.toLowerCase()}-1`" />
+            <div class="flex basis-full"></div>
+        </div>
+        <SubMenu :menu-items="subMenuCtx" />
+    </div>
+    <div class="divider"></div>
+    <QuickNav :menu-items="subMenuCtx" />
+</template>
+
+<script lang="ts" setup>
+const name = "GetReqHeaders";
+const title = `Fiber | 🧠 Ctx - ${name}`;
+const description = "Returns the HTTP request headers.";
+
+useSeoMeta({
+    title: title,
+    ogTitle: title,
+    description: description,
+    ogDescription: description,
+});
+</script>
