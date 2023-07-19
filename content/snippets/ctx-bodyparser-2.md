8c13975aa7e27f5fb3b7356e5b9ea8e3911b40b8
kirari04 <lev@saminskij.ch>
2023-07-19 19:19:00 +0200

    added body parse

---
+++ content/snippets/ctx-bodyparser-2.md
@@ -0,0 +1,32 @@
+```go
+// Field names should start with an uppercase letter
+type Person struct {
+    Name string `json:"name" xml:"name" form:"name"`
+    Pass string `json:"pass" xml:"pass" form:"pass"`
+}
+
+app.Post("/", func(c *fiber.Ctx) error {
+        p := new(Person)
+
+        if err := c.BodyParser(p); err != nil {
+            return err
+        }
+
+        log.Println(p.Name) // john
+        log.Println(p.Pass) // doe
+
+        // ...
+})
+
+// Run tests with the following curl commands
+
+// curl -X POST -H "Content-Type: application/json" --data "{\"name\":\"john\",\"pass\":\"doe\"}" localhost:3000
+
+// curl -X POST -H "Content-Type: application/xml" --data "<login><name>john</name><pass>doe</pass></login>" localhost:3000
+
+// curl -X POST -H "Content-Type: application/x-www-form-urlencoded" --data "name=john&pass=doe" localhost:3000
+
+// curl -X POST -F name=john -F pass=doe http://localhost:3000
+
+// curl -X POST "http://localhost:3000/?name=john&pass=doe"
+```
---
+++ pages/docs/api/ctx/bodyparser.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="flex flex-col-reverse md:flex-row grow">
+        <div class="flex flex-col grow">
+            <H1>🧠 Ctx - {{ name }}</H1>
+            <P> Binds the request body to a struct. </P>
+            <P>
+                It is important to specify the correct struct tag based on the
+                content type to be parsed. For example, if you want to parse a
+                <B>JSON body</B> with a field called Pass, you would use a
+                struct field of <Tag>json:"pass"</Tag>.
+            </P>
+            <div class="my-6">
+                <div class="overflow-x-auto">
+                    <table class="table">
+                        <!-- head -->
+                        <thead>
+                            <tr>
+                                <th>content-type</th>
+                                <th>struct tag</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>application/x-www-form-urlencoded</th>
+                                <td><Tag>form</Tag></td>
+                            </tr>
+                            <tr>
+                                <th>multipart/form-data</th>
+                                <td><Tag>form</Tag></td>
+                            </tr>
+                            <tr>
+                                <th>application/json</th>
+                                <td><Tag>json</Tag></td>
+                            </tr>
+                            <tr>
+                                <th>text/xml</th>
+                                <td><Tag>xml</Tag></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <Code :path="`/snippets/ctx-${name.toLowerCase()}-1`" />
+            <P>
+                Example implementation of <B>{{ name }}</B>
+            </P>
+            <Code :path="`/snippets/ctx-${name.toLowerCase()}-2`" />
+            <div class="alert alert-info mt-6">
+                <Icon icon="info" />
+                <span>
+                    Returned value is only valid within the handler. Do not
+                    store any references. Make copies or use the
+                    <B>Immutable</B> setting instead.
+                </span>
+                <nuxtLink to="/docs/welcome/zero-allocation" class="btn">
+                    Learn more
+                </nuxtLink>
+            </div>
+            <div class="flex basis-full"></div>
+        </div>
+        <SubMenu :menu-items="subMenuCtx" />
+    </div>
+    <div class="divider"></div>
+    <QuickNav :menu-items="subMenuCtx" />
+</template>
+
+<script lang="ts" setup>
+const name = "BodyParser";
+const title = `Fiber | 🧠 Ctx - ${name}`;
+const description = "Binds the request body to a struct.";
+
+useSeoMeta({
+    title: title,
+    ogTitle: title,
+    description: description,
+    ogDescription: description,
+});
+</script>
