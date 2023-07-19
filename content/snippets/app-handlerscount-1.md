860cd28cbeb7c2b75e6e5bded970d6e2ff63acf6
kirari04 <lev@saminskij.ch>
2023-07-19 10:11:20 +0200

    addded app HandlersCount
    Signed-off-by: kirari04 <lev@saminskij.ch>

---
+++ content/snippets/app-handlerscount-1.md
@@ -0,0 +1,4 @@
+```go
+// Function signature
+func (app *App) HandlersCount() uint32
+```
---
+++ pages/api/app/handlerscount.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="flex flex-col-reverse md:flex-row grow">
+        <div class="flex flex-col grow">
+            <H1>🚀 App - HandlersCount</H1>
+            <P> This method returns the amount of registered handlers. </P>
+            <Code path="/snippets/app-handlerscount-1" />
+            <div class="flex basis-full"></div>
+        </div>
+        <SubMenu :menu-items="subMenuApp" />
+    </div>
+    <div class="divider"></div>
+    <QuickNav :menu-items="subMenuApp" />
+</template>
+
+<script lang="ts" setup>
+const title = "Fiber | 🚀 App - HandlersCount";
+const description = "This method returns the amount of registered handlers.";
+
+useSeoMeta({
+    title: title,
+    ogTitle: title,
+    description: description,
+    ogDescription: description,
+});
+</script>
