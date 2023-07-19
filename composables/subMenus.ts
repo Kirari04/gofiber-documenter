4dd9d1f5d72c93cb013ef94129251967f040aba0
kirari04 <lev@saminskij.ch>
2023-07-19 18:47:14 +0200

    added all sub menu items

--- composables/subMenus.ts
+++ composables/subMenus.ts
@@ -148,103 +148,322 @@ export const subMenuApp: Array<{
     },
 ];
 
-export const subMenuCtx: Array<{
-    href: string;
-    name: string;
-}> = [
+const subMenuCtx = [
     {
-        href: "/docs/api/ctx/accepts",
-        name: "Accepts",
+      href: "/docs/api/ctx/accepts",
+      name: "Accepts",
     },
     {
-        href: "/docs/api/ctx/allparams",
-        name: "AllParams",
+      href: "/docs/api/ctx/allparams",
+      name: "AllParams",
     },
     {
-        href: "/docs/api/ctx/app",
-        name: "App",
+      href: "/docs/api/ctx/app",
+      name: "App",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/append",
+      name: "Append",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/attachment",
+      name: "Attachment",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/baseurl",
+      name: "BaseURL",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/bind",
+      name: "Bind",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/body",
+      name: "Body",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/bodyparser",
+      name: "BodyParser",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/clearcookie",
+      name: "ClearCookie",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/clienthelloinfo",
+      name: "ClientHelloInfo",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/context",
+      name: "Context",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/cookie",
+      name: "Cookie",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/cookies",
+      name: "Cookies",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/download",
+      name: "Download",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/format",
+      name: "Format",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/formfile",
+      name: "FormFile",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/formvalue",
+      name: "FormValue",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/fresh",
+      name: "Fresh",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/get",
+      name: "Get",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
-    },{
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/getreqheaders",
+      name: "GetReqHeaders",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/getrespheader",
+      name: "GetRespHeader",
     },
     {
-        href: "/docs/api/ctx/xx",
-        name: "xxx",
+      href: "/docs/api/ctx/getrespheaders",
+      name: "GetRespHeaders",
     },
-];
+    {
+      href: "/docs/api/ctx/getrouteurl",
+      name: "GetRouteURL",
+    },
+    {
+      href: "/docs/api/ctx/hostname",
+      name: "Hostname",
+    },
+    {
+      href: "/docs/api/ctx/ip",
+      name: "IP",
+    },
+    {
+      href: "/docs/api/ctx/ips",
+      name: "IPs",
+    },
+    {
+      href: "/docs/api/ctx/is",
+      name: "Is",
+    },
+    {
+      href: "/docs/api/ctx/isfromlocal",
+      name: "IsFromLocal",
+    },
+    {
+      href: "/docs/api/ctx/json",
+      name: "JSON",
+    },
+    {
+      href: "/docs/api/ctx/jsonp",
+      name: "JSONP",
+    },
+    {
+      href: "/docs/api/ctx/links",
+      name: "Links",
+    },
+    {
+      href: "/docs/api/ctx/locals",
+      name: "Locals",
+    },
+    {
+      href: "/docs/api/ctx/location",
+      name: "Location",
+    },
+    {
+      href: "/docs/api/ctx/method",
+      name: "Method",
+    },
+    {
+      href: "/docs/api/ctx/multipartform",
+      name: "MultipartForm",
+    },
+    {
+      href: "/docs/api/ctx/next",
+      name: "Next",
+    },
+    {
+      href: "/docs/api/ctx/originalurl",
+      name: "OriginalURL",
+    },
+    {
+      href: "/docs/api/ctx/params",
+      name: "Params",
+    },
+    {
+      href: "/docs/api/ctx/paramsint",
+      name: "ParamsInt",
+    },
+    {
+      href: "/docs/api/ctx/paramsparser",
+      name: "ParamsParser",
+    },
+    {
+      href: "/docs/api/ctx/path",
+      name: "Path",
+    },
+    {
+      href: "/docs/api/ctx/protocol",
+      name: "Protocol",
+    },
+    {
+      href: "/docs/api/ctx/queries",
+      name: "Queries",
+    },
+    {
+      href: "/docs/api/ctx/query",
+      name: "Query",
+    },
+    {
+      href: "/docs/api/ctx/querybool",
+      name: "QueryBool",
+    },
+    {
+      href: "/docs/api/ctx/queryfloat",
+      name: "QueryFloat",
+    },
+    {
+      href: "/docs/api/ctx/queryint",
+      name: "QueryInt",
+    },
+    {
+      href: "/docs/api/ctx/queryparser",
+      name: "QueryParser",
+    },
+    {
+      href: "/docs/api/ctx/range",
+      name: "Range",
+    },
+    {
+      href: "/docs/api/ctx/redirect",
+      name: "Redirect",
+    },
+    {
+      href: "/docs/api/ctx/redirecttoroute",
+      name: "RedirectToRoute",
+    },
+    {
+      href: "/docs/api/ctx/redirectback",
+      name: "RedirectBack",
+    },
+    {
+      href: "/docs/api/ctx/render",
+      name: "Render",
+    },
+    {
+      href: "/docs/api/ctx/request",
+      name: "Request",
+    },
+    {
+      href: "/docs/api/ctx/reqheaderparser",
+      name: "ReqHeaderParser",
+    },
+    {
+      href: "/docs/api/ctx/response",
+      name: "Response",
+    },
+    {
+      href: "/docs/api/ctx/restartrouting",
+      name: "RestartRouting",
+    },
+    {
+      href: "/docs/api/ctx/route",
+      name: "Route",
+    },
+    {
+      href: "/docs/api/ctx/savefile",
+      name: "SaveFile",
+    },
+    {
+      href: "/docs/api/ctx/savefiletostorage",
+      name: "SaveFileToStorage",
+    },
+    {
+      href: "/docs/api/ctx/secure",
+      name: "Secure",
+    },
+    {
+      href: "/docs/api/ctx/send",
+      name: "Send",
+    },
+    {
+      href: "/docs/api/ctx/sendfile",
+      name: "SendFile",
+    },
+    {
+      href: "/docs/api/ctx/sendstatus",
+      name: "SendStatus",
+    },
+    {
+      href: "/docs/api/ctx/set",
+      name: "Set",
+    },
+    {
+      href: "/docs/api/ctx/setparserdecoder",
+      name: "SetParserDecoder",
+    },
+    {
+      href: "/docs/api/ctx/setusercontext",
+      name: "SetUserContext",
+    },
+    {
+      href: "/docs/api/ctx/stale",
+      name: "Stale",
+    },
+    {
+      href: "/docs/api/ctx/status",
+      name: "Status",
+    },
+    {
+      href: "/docs/api/ctx/subdomains",
+      name: "Subdomains",
+    },
+    {
+      href: "/docs/api/ctx/type",
+      name: "Type",
+    },
+    {
+      href: "/docs/api/ctx/usercontext",
+      name: "UserContext",
+    },
+    {
+      href: "/docs/api/ctx/vary",
+      name: "Vary",
+    },
+    {
+      href: "/docs/api/ctx/write",
+      name: "Write",
+    },
+    {
+      href: "/docs/api/ctx/writef",
+      name: "Writef",
+    },
+    {
+      href: "/docs/api/ctx/writestring",
+      name: "WriteString",
+    },
+    {
+      href: "/docs/api/ctx/xhr",
+      name: "XHR",
+    },
+    {
+      href: "/docs/api/ctx/xml",
+      name: "XML",
+    },
+  ];
+  
