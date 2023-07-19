d5cfba090a1e42ea65539440390d3477dd0df44c
kirari04 <lev@saminskij.ch>
2023-07-19 17:20:27 +0200

    fixed text overflow with long names

--- components/H1.vue
+++ components/H1.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1 class="text-4xl md:text-6xl py-4 font-bold">
+    <h1 class="text-4xl md:text-6xl py-4 font-bold break-words shrink md:min-w-full md:w-0">
         <slot />
     </h1>
 </template>
