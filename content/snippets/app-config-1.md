f3d9260de33c27adbe5f19cb831f0283a35fbbb6
kirari04 <lev@saminskij.ch>
2023-07-19 10:45:39 +0200

    added config
    Signed-off-by: kirari04 <lev@saminskij.ch>

---
+++ content/snippets/app-config-1.md
@@ -0,0 +1,4 @@
+```go
+// Function signature
+func (app *App) Config() Config
+```
---
+++ pages/api/app/config.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="flex flex-col-reverse md:flex-row grow">
+        <div class="flex flex-col grow">
+            <H1>🚀 App - Config</H1>
+            <P> Config returns the app config as value ( read-only ). </P>
+            <Code path="/snippets/app-config-1" />
+            <div class="flex basis-full"></div>
+        </div>
+        <SubMenu :menu-items="subMenuApp" />
+    </div>
+    <div class="divider"></div>
+    <QuickNav :menu-items="subMenuApp" />
+</template>
+
+<script lang="ts" setup>
+const title = "Fiber | 🚀 App - Config";
+const description = "Config returns the app config as value ( read-only ).";
+
+useSeoMeta({
+    title: title,
+    ogTitle: title,
+    description: description,
+    ogDescription: description,
+});
+</script>
