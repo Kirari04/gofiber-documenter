3834cee99a91ca21a96fb2e65cfe89357f07ac0e
kirari04 <lev@saminskij.ch>
2023-07-19 08:43:54 +0200

    added subnavigation for app
    Signed-off-by: kirari04 <lev@saminskij.ch>

--- composables/subMenus.ts
+++ composables/subMenus.ts
@@ -49,3 +49,101 @@ export const subMenuFiber: Array<{
         name: "IsChild",
     },
 ];
+
+export const subMenuApp: Array<{
+    href: string;
+    name: string;
+}> = [
+    {
+        href: "/api/app/static",
+        name: "Static",
+    },
+    {
+        href: "/api/app/route-handlers",
+        name: "Route Handlers",
+    },
+    {
+        href: "/api/app/mount",
+        name: "Mount",
+    },
+    {
+        href: "/api/app/mountpath",
+        name: "MountPath",
+    },
+    {
+        href: "/api/app/group",
+        name: "Group",
+    },
+    {
+        href: "/api/app/route",
+        name: "Route",
+    },
+    {
+        href: "/api/app/server",
+        name: "Server",
+    },
+    {
+        href: "/api/app/server-shutdown",
+        name: "Server Shutdown",
+    },
+    {
+        href: "/api/app/handlerscount",
+        name: "HandlersCount",
+    },
+    {
+        href: "/api/app/stack",
+        name: "Stack",
+    },
+    {
+        href: "/api/app/name",
+        name: "Name",
+    },
+    {
+        href: "/api/app/getroute",
+        name: "GetRoute",
+    },
+    {
+        href: "/api/app/getroutes",
+        name: "GetRoutes",
+    },
+    {
+        href: "/api/app/config",
+        name: "Config",
+    },
+    {
+        href: "/api/app/handler",
+        name: "Handler",
+    },
+    {
+        href: "/api/app/listen",
+        name: "Listen",
+    },
+    {
+        href: "/api/app/listentls",
+        name: "ListenTLS",
+    },
+    {
+        href: "/api/app/listentlswithcertificate",
+        name: "ListenTLSWithCertificate",
+    },
+    {
+        href: "/api/app/listenmutualtls",
+        name: "ListenMutualTLS",
+    },
+    {
+        href: "/api/app/ListenMutualTLSWithCertificate",
+        name: "ListenMutualTLSWithCertificate",
+    },
+    {
+        href: "/api/app/listener",
+        name: "Listener",
+    },
+    {
+        href: "/api/app/test",
+        name: "Test",
+    },
+    {
+        href: "/api/app/hooks",
+        name: "Hooks",
+    },
+];
