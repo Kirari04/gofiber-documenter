a138a9549b1dec87bf2a38a28b3942a359540e98
kirari04 <lev@saminskij.ch>
2023-07-19 10:46:47 +0200

    added handler
    Signed-off-by: kirari04 <lev@saminskij.ch>

---
+++ content/snippets/app-handler-1.md
@@ -0,0 +1,4 @@
+```go
+// Function signature
+func (app *App) Handler() fasthttp.RequestHandler
+```
---
+++ pages/api/app/handler.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex flex-col-reverse md:flex-row grow">
+        <div class="flex flex-col grow">
+            <H1>🚀 App - Handler</H1>
+            <P>
+                Handler returns the server handler that can be used to serve
+                custom *fasthttp.RequestCtx requests.
+            </P>
+            <Code path="/snippets/app-handler-1" />
+            <div class="flex basis-full"></div>
+        </div>
+        <SubMenu :menu-items="subMenuApp" />
+    </div>
+    <div class="divider"></div>
+    <QuickNav :menu-items="subMenuApp" />
+</template>
+
+<script lang="ts" setup>
+const title = "Fiber | 🚀 App - Handler";
+const description =
+    "Handler returns the server handler that can be used to serve custom *fasthttp.RequestCtx requests.";
+
+useSeoMeta({
+    title: title,
+    ogTitle: title,
+    description: description,
+    ogDescription: description,
+});
+</script>
