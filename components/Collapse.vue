506aefc05cba0f5f555fa754636179b774fef08e
kirari04 <lev@saminskij.ch>
2023-07-19 07:12:22 +0200

    added wrapping and text overflow
    Signed-off-by: kirari04 <lev@saminskij.ch>

--- components/Collapse.vue
+++ components/Collapse.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="collapse collapse-arrow border border-base-300 bg-base-200">
         <input type="checkbox" class="peer" />
-        <div class="collapse-title text-xl font-medium flex items-center gap-2">
+        <div
+            class="collapse-title text-xl font-medium flex items-center gap-2 flex-wrap truncate"
+        >
             <slot name="title" />
         </div>
         <div class="collapse-content">
